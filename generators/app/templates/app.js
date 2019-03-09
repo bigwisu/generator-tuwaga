7bfe236bdec9ca3a78a355c0043147f667525da9
Wisu Suntoyo <bigwisu@gmail.com>
2019-03-09 09:04:22 +0700

    add body parser

--- generators/app/templates/app.js
+++ generators/app/templates/app.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const express = require('express');
 const enrouten = require('express-enrouten');
+const bodyParser = require('body-parser');
 const config = require('./config');
 
 /* eslint-disable no-console */
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use('/<%= serviceApiRoot %>', enrouten({
   directory: path.join(__dirname, 'controllers'),
